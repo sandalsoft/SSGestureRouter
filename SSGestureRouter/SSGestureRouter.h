c88b48da47d63d72d376a89bd7872bada1b24882
Eric Nelson <eric@sandalsoft.com>
2013-05-03 18:17:58 -0500

    Initial Commit

---
+++ SSGestureRouter/SSGestureRouter.h
@@ -0,0 +1,13 @@
+//
+//  SSGestureRouter.h
+//  SSGestureRouter
+//
+//  Created by Eric Nelson on 5/3/13.
+//  Copyright (c) 2013 Sandalsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SSGestureRouter : NSObject
+
+@end
---
+++ SSGestureRouter/SSGestureRouter.m
@@ -0,0 +1,13 @@
+//
+//  SSGestureRouter.m
+//  SSGestureRouter
+//
+//  Created by Eric Nelson on 5/3/13.
+//  Copyright (c) 2013 Sandalsoft. All rights reserved.
+//
+
+#import "SSGestureRouter.h"
+
+@implementation SSGestureRouter
+
+@end
